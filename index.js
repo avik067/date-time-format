a716236f77888ff58cb12541a74ef4fe7524e755
avik <avikbiswas067>
2023-06-07 14:26:58 +0000

    second

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const add = require("date-fns/addDays");
-let f = (x) => {
-  const result = add(new Date(2020, 8, 22), x);
 
-  //   console.log(
-  //     `${result.getDay()}-${result.getMonth()}-${result.getFullYear()}`
-  //   );
-  return `${result.getDay()}-${result.getMonth()}-${result.getFullYear()}`;
+let f = (x) => {
+  const result = add(new Date(2020, 7, 22), x);
+
+  console.log(
+    `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`
+  );
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
 };
-// f(10);
+f(10);
 module.exports = f;
